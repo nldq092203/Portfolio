695260787f631545f3046f0fc774341ef88afd37
nldq092203 <lnguye220903@gmail.com>
2024-11-11 16:37:42 +0100

    Add CV version English

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,34 +1,43 @@
-
-import cv from "../../assets/CV.jpg";
-import { Container,Image, Button } from "@mantine/core";
+import cvEnglish from "../../assets/CV_en.jpg";
+import cvFrench from "../../assets/CV_fr.jpg";
+import { Container, Image, Button, Title } from "@mantine/core";
 import Particle from "../Particle";
 import { IconDownload } from '@tabler/icons-react';
 import { useTranslation } from 'react-i18next';
+
 function Resume() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  
+  // Determine the current language
+  const isFrench = i18n.language === 'fr';
+
   return (
     <Container fluid className="project-section" style={{ position: "relative" }}>
       <Particle />
-      <Image 
-        src={cv} 
-        className="img-fluid" 
-        alt="CV" 
-        fit="contain" 
-        radius="xl" 
-        w="60%" 
-        h="auto" 
-        style={{ margin: "50px auto", display: "block" }}
-      />    
-    <Button
-      component="a"
-      href="/CV.pdf" // Direct path to the PDF file in the public folder
-      target="_blank"
-      rel="noopener noreferrer"
-      leftSection={<IconDownload />}
-      variant="outline"
-    >
-     {t("resume.download")}
-    </Button>
+
+      {/* Display the CV image and download button based on the current language */}
+      <Container style={{ textAlign: "center" }}>        
+        <Image
+          src={isFrench ? cvFrench : cvEnglish}
+          alt={isFrench ? "CV French" : "CV English"}
+          fit="contain"
+          radius="xl"
+          w="60%"
+          h="auto"
+          style={{ margin: "20px auto" }}
+        />
+        
+        <Button
+          component="a"
+          href={isFrench ? "/CV_fr.pdf" : "/CV_en.pdf"} 
+          target="_blank"
+          rel="noopener noreferrer"
+          leftSection={<IconDownload />}
+          variant="outline"
+        >
+          {t("resume.download")}
+        </Button>
+      </Container>
     </Container>
   );
 }
